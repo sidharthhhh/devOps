e81391c534e361aa2d7d382009bd22d6a9598d4b
Sidharth Shukla <swayam1044@gmail.com>
2023-08-17 10:56:33 +0530

    variables

---
+++ Go/day1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+//    var batman string = "i m batman";
+//    fmt.Println(batman)
+
+//    var superman string
+//    superman ="i m superman";
+//    fmt.Println(superman);
+
+//    thor:= "i m thor";
+//    fmt.Printf(thor);
+
+//    thorRating:= 3.0
+//    fmt.Printf("%v, %T", thorRating, thorRating);
+
+//    var Ironman, catAmerica string = " i m ironman", "i m captain america";
+//    fmt.Println(Ironman, catAmerica)
+
+//    var defaultValue string
+//    fmt.Println(defaultValue)
+
+//    var (
+// 	spiderman = "i m spider man"
+// 	age = 16
+// 	powers = "web shooter, spider sense"
+// 	antman = "i m antman"
+//    )
+//    fmt.Println(spiderman,age,powers,antman)
+
+
+
+// new start
+
+var username string = "sidharth";
+fmt.Println(username)
+fmt.Printf("variable is of type : %T \n", username)
+
+
+var isLoggedIn bool = true
+fmt.Println(isLoggedIn)
+fmt.Printf("variable is of type : %T \n", isLoggedIn)
+
+
+var small uint8 = 255;
+fmt.Println(small)
+fmt.Printf("variable is of type : %T \n", small)
+}
