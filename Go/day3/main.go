1a5018f09a48f247ab73dcdbd8379e5ef74bed18
Sidharth Shukla <swayam1044@gmail.com>
2023-08-26 23:32:40 +0530

    shellscrpting

---
+++ Go/day3/main.go
@@ -0,0 +1,17 @@
+package main;
+
+import "fmt";
+
+func main(){
+	// messageLen := 10
+	// maxMessageLen := 20
+	// fmt.Println("trying to send length of message: ", messageLen);
+
+	// if messageLen <= maxMessageLen{
+	// 	fmt.Println("message sent")
+	// }else{
+	// 	fmt.Println("message not send")
+	// }
+
+  
+}
